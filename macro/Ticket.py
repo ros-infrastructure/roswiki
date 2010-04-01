9a3fbe52b4014da2cc0e04ef4c77a7fc945ad8e9
kwc <kwc@willowgarage.com>
2010-04-01 22:59:32 +0000

    #2571 making slightly more robust
    git-svn-id: https://code.ros.org/svn/ros/stacks/rosorg/trunk/roswiki@8893 61973afe-1cd6-434e-a0a9-934cb0052259

--- macro/Ticket.py
+++ macro/Ticket.py
@@ -1,6 +1,12 @@
 Dependencies = []
 
 def execute(macro, args):
-  project = args.split()[0]
-  num = args.split()[1]
-  return macro.formatter.text("Use trac to ") + macro.formatter.url(1, "https://code.ros.org/trac/%s/ticket/%s"%(project, num)) + macro.formatter.text("#%s"%(num)) + macro.formatter.url(0) + macro.formatter.linebreak(0)
+  if ',' in args:
+    args = args.split(',')
+  else:
+    args = args.split()
+  project = args0]
+  num = args[1]
+  if num[0] == '#':
+    num = num[1:]
+  return macro.formatter.text("Use trac to ") + macro.formatter.url(1, "https://code.ros.org/trac/%s/ticket/%s"%(project, num)) + macro.formatter.text("#%s"%(num)) + macro.formatter.url(0) 
