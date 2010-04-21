55c60ab9d7444309715c4610f43834a4c2cff9a4
kwc <kwc@willowgarage.com>
2010-04-21 00:24:11 +0000

    ticket macro more robust
    git-svn-id: https://code.ros.org/svn/ros/stacks/rosorg/trunk/roswiki@9270 61973afe-1cd6-434e-a0a9-934cb0052259

--- macro/Ticket.py
+++ macro/Ticket.py
@@ -5,8 +5,11 @@ def execute(macro, args):
     args = args.split(',')
   else:
     args = args.split()
-  project = args[0]
-  num = args[1]
+  try:
+    project = args[0]
+    num = args[1]
+  except:
+    return "Invalid ticket args [%s]"%(str(args))
   if num[0] == '#':
     num = num[1:]
   return macro.formatter.url(1, "https://code.ros.org/trac/%s/ticket/%s"%(project, num)) + macro.formatter.text("#%s"%(num)) + macro.formatter.url(0) 
