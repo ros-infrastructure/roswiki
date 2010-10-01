aa65fbb9500fbc20a6164f319977c4ebdec59f2b
wim <wim>
2010-10-01 02:30:49 +0000

    update version macro for diamondback
    git-svn-id: https://code.ros.org/svn/ros/stacks/rosorg/trunk/roswiki@11360 61973afe-1cd6-434e-a0a9-934cb0052259

--- macro/Version.py
+++ macro/Version.py
@@ -2,4 +2,7 @@ Dependencies = []
 
 def execute(macro, args):
   version = str(args)
-  return '<span style="background-color:#FFFF00; font-weight:bold; padding: 3px;">New in %s</span>'%version
+  if version == 'D' or version == 'D Turtle' or version == 'Diamondback':
+    return '<span style="background-color:#FFFF00; font-weight:bold; padding: 3px;">Expected in Diamondback (unstable)</span>'
+  else:
+    return '<span style="background-color:#FFFF00; font-weight:bold; padding: 3px;">New in %s</span>'%version
