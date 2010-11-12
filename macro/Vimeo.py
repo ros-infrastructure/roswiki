771a8fa263d2c77674ecf938c853fe0008f630fb
kwc <kwc@willowgarage.com>
2010-11-12 00:18:02 +0000

    new update from timn
    git-svn-id: https://code.ros.org/svn/ros/stacks/rosorg/trunk/roswiki@12169 61973afe-1cd6-434e-a0a9-934cb0052259

--- macro/Vimeo.py
+++ macro/Vimeo.py
@@ -2,7 +2,7 @@ Dependencies = []
 
 def execute(macro, args):
   key = args.split()[0]
-  if key.startswith('http://player.vimeo.com/video/'):
-    key = key[len('http://player.vimeo.com/video/'):]
-  return macro.formatter.rawHTML('<iframe src="http://player.vimeo.com/video/%(key)" width="400" height="300" frameborder="0"></iframe>'%locals())
+  if key.startswith('http://www.vimeo.com/'):
+    key = key[len('http://www.vimeo.com/'):]
+  return macro.formatter.rawHTML('<center><iframe src="http://player.vimeo.com/video/%(key)s" width="400" height="300" frameborder="0"></iframe></center>'%locals())
 
