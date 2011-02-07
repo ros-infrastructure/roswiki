8624a1e0ea0c5bee72ec91e4024482dec74f312a
kwc <kwc@willowgarage.com>
2011-02-07 17:44:37 +0000

    first full prototype
    git-svn-id: https://code.ros.org/svn/ros/stacks/rosorg/trunk/roswiki@13103 61973afe-1cd6-434e-a0a9-934cb0052259

--- macro/LurkerSearch.py
+++ macro/LurkerSearch.py
@@ -1,8 +1,9 @@
 Dependencies = []
 
 def execute(macro, args):
-  keys = args.split()[0]
-  for key in keys:
-     if '<' in key or '>' in key or '"' in key:
-        return "Invalid Lurker argument"
-  return macro.formatter.rawHTML('<a href="https://code.ros.org/lurker/search/20380101.000000.00000000@%s.en.html">search</a>'%(','.join(keys)))
+    keys = args.split(' ')
+    for key in keys:
+        if '<' in key or '>' in key or '"' in key:
+            return "Invalid Lurker argument"
+    p = ','.join(keys)
+    return macro.formatter.rawHTML('<a class="mailto" title="Search list archives for \'%s\'" href="https://code.ros.org/lurker/search/20380101.000000.00000000@%s.en.html">[ML]</a>'%(p, p))
