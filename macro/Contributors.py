272a9790d423c48d9902663125c9fcf991100c1c
kwc <kwc@willowgarage.com>
2011-08-12 17:37:34 +0000

    Contributors list
    git-svn-id: https://code.ros.org/svn/ros/stacks/rosorg/trunk/roswiki@14653 61973afe-1cd6-434e-a0a9-934cb0052259

---
+++ macro/Contributors.py
@@ -0,0 +1,8 @@
+from __future__ import with_statement
+generates_headings = True
+dependencies = []
+
+import codecs
+def macro_Contributors(macro):
+    with codecs.open('/var/www/www.ros.org/html/contributors.html', 'r', encoding='utf-8') as f:
+        return f.read()
