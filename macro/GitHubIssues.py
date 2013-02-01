c1ee21f876df6681e4faea9f813026c13288cd0b
Tully Foote <tfoote@willowgarage.com>
2013-01-31 16:51:44 -0800

    Create GitHubIssues.py macro
    fixes #9

---
+++ macro/GitHubIssues.py
@@ -0,0 +1,24 @@
+Dependencies = []
+
+def execute(macro, args):
+  split_args = args.split()
+  project = split_args[0]
+  if len(split_args) >= 2:
+    component = split_args[1]
+  else:
+    component = None
+
+
+  f = macro.formatter
+  text = f.text
+  url = f.url
+  base = "https://github.com/%s/issues"%(project)
+  base_q = base + "?page=1&state=open"
+  base_new = base + "/new"
+
+  output = text("Use GitHub to ") + url(1, base_new) + text("report bugs or submit feature requests") + url(0)  + text(". ")
+  if component:
+    output += text("Please use '%s:' at the beginning of the title. "%component)
+
+  output += text("[") + url(1,base_q) + text("View active issues") + url(0) + text("]") + f.linebreak(0)
+  return output
