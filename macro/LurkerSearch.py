b3dd851abae90afe184615ea15bf1cdb11fda9df
Dirk Thomas <dthomas@osrfoundation.org>
2013-12-01 15:50:29 -0800

    remove unused lurker macros (fix #58)

--- macro/LurkerSearch.py
+++
@@ -1,9 +0,0 @@
-Dependencies = []
-
-def execute(macro, args):
-  keys = args.split(' ')
-  for key in keys:
-     if '<' in key or '>' in key or '"' in key:
-        return "Invalid Lurker argument"
-  p = ','.join(keys)
-  return macro.formatter.rawHTML('<a class="mailto" title="Search list archives for \'%s\'" href="https://code.ros.org/lurker/search/20380101.000000.00000000@%s.en.html">[ML]</a>'%(p, p))
