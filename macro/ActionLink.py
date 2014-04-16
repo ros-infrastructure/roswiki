b7fff1af10581dd1c48841769ae9b00e1a2e93ab
agentx3r <paul@bovbel.com>
2014-04-16 12:32:42 -0400

    add action link macro

---
+++ macro/ActionLink.py
@@ -0,0 +1,11 @@
+Dependencies = []
+
+def execute(macro, args):
+  if (args[0] == '<' and args[-1] == '>'):
+    return macro.formatter.emphasis(1) + macro.formatter.code(1) + macro.formatter.text(args) + macro.formatter.code(0) + macro.formatter.emphasis(0)
+  type_ = args.split()[0]
+  splits = type_.split('/')
+  if len(splits) != 2:
+    return "invalid message type for ActionLink(action/type)"
+  pkg, base = splits  
+  return macro.formatter.url(1, "http://docs.ros.org/api/%s/html/action/%s.html"%(pkg, base)) + macro.formatter.text(type_)+ macro.formatter.url(0)
