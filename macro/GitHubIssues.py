d1ea157a4cee6878b753fc80f3f036d71c4db281
G.A. vd. Hoorn <g.a.vanderhoorn@tudelft.nl>
2015-06-16 11:09:11 +0200

    Update GitHubIssues macro text to mention 'open PRs'.
    The text generated by the GitHubIssues macro is incomplete: Github will
    not only show open issues, but open PRs as well. This change updates
    the text to reflect that.
    
    Also includes a minor change to the generated URL, based on the syntax
    described in [1] and [2].
    
    [1] github help - Search / Searching issues
        https://help.github.com/articles/searching-issues
    [2] github help - Managing projects / Sharing filters
        https://help.github.com/articles/sharing-filters

--- macro/GitHubIssues.py
+++ macro/GitHubIssues.py
@@ -13,12 +13,12 @@ def execute(macro, args):
   text = f.text
   url = f.url
   base = "https://github.com/%s/issues"%(project)
-  base_q = base + "?page=1&state=open"
+  base_q = base + "?page=1&q=state:open"
   base_new = base + "/new"
 
   output = text("Use GitHub to ") + url(1, base_new) + text("report bugs or submit feature requests") + url(0)  + text(". ")
   if component:
     output += text("Please use '%s:' at the beginning of the title. "%component)
 
-  output += text("[") + url(1,base_q) + text("View active issues") + url(0) + text("]") + f.linebreak(0)
+  output += text("[") + url(1,base_q) + text("View active issues and open PRs") + url(0) + text("]") + f.linebreak(0)
   return output
