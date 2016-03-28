3b735866695020767df98b68c536fe6e3abbfb28
Jiri Horner <laeqten@gmail.com>
2016-03-28 20:23:15 +0200

    use newest google embedded API for youtube
    this allows to use HTML5 instead of flash.

--- macro/Youtube.py
+++ macro/Youtube.py
@@ -4,4 +4,4 @@ def execute(macro, args):
   key = args.split()[0]
   if key.startswith('http://www.youtube.com/watch?v='):
     key = key[len('http://www.youtube.com/watch?v='):]
-  return macro.formatter.rawHTML('<object width="480" height="295"><param name="movie" value="http://www.youtube.com/v/%(key)s&hl=en_US&fs=1&hd=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/%(key)s&hl=en_US&fs=1&hd=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="480" height="295"></embed></object>'%locals())
+  return macro.formatter.rawHTML('<center><iframe type="text/html" width="480" height="270" src="http://www.youtube.com/embed/%(key)s" frameborder="0" /></center>'%locals())
