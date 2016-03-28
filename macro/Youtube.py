98abaf6de7c3e62a5b9c812baaef7fb62c3b2df1
Jiri Horner <laeqten@gmail.com>
2016-03-28 23:59:35 +0200

    youtube: backward compatibility with wiki
    flash version didn't mind if first query paramt was preceded by &. This version requires query params to be properly formatted. Passing param with & was posted as tip on the wiki so we are preserving this behavior.

--- macro/Youtube.py
+++ macro/Youtube.py
@@ -4,4 +4,5 @@ def execute(macro, args):
   key = args.split()[0]
   if key.startswith('http://www.youtube.com/watch?v='):
     key = key[len('http://www.youtube.com/watch?v='):]
+  key = key.replace('&', '?', 1) # query params must start with ?
   return macro.formatter.rawHTML('<center><iframe type="text/html" width="480" height="270" src="http://www.youtube.com/embed/%(key)s" frameborder="0" allowfullscreen /></center>'%locals())
