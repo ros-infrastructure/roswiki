1a62c4cc73257b2914f82df99c41d9a208fe5ef4
Mikael Arguedas <mikael.arguedas@gmail.com>
2017-01-25 07:50:16 -0800

    comprehension list 2nd round

--- macro/StackHeader.py
+++ macro/StackHeader.py
@@ -10,9 +10,7 @@ dependencies = []
 
 hidden_distros = ['boxturtle', 'cturtle', 'diamondback', 'electric', 'fuerte', 'groovy', 'hydro', 'unstable']
 
-for hidden_distro in hidden_distros if hidden_distro in distro_names:
-    distro_names.remove(hidden_distro)
-
+distro_names = [d for d in distro_names if d not in hidden_distros]
 
 def generate_old_stack_header(macro, stack_name, opt_distro=None):
     try:
