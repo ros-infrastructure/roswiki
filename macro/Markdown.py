85d4990b089db83909e60ff0b9544c2b263c04ab
David V. Lu <davidvlu@gmail.com>
2018-12-07 10:20:05 -0500

    Markdown macro

---
+++ macro/Markdown.py
@@ -0,0 +1,17 @@
+import pypandoc
+import requests
+import re
+
+GITHUB_PATTERN = re.compile(r'https?://w*\.?github.com/([^/]+)/([^/]+)/blob/([^/]+)/([^/]+.md)')
+RAW_GITHUB_TEMPLATE = 'https://raw.githubusercontent.com/%s/%s/%s/%s'
+
+def macro_Markdown(macro, arg1):
+    content = ''
+    if arg1 is not None:
+        m = GITHUB_PATTERN.match(arg1)
+        if m:
+            arg1 = RAW_GITHUB_TEMPLATE % m.groups()
+        r = requests.get(arg1)
+        content = r.text
+    output = pypandoc.convert_text(content, 'mediawiki', 'md')
+    return output
