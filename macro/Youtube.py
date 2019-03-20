e895df7097e70fff209d9efeccb01ead1e60d653
Tully Foote <tfoote@osrfoundation.org>
2019-03-20 16:10:57 -0700

    switch to use the nocookie private version of the youtube embed
    This eliminates a lot of trackers.

--- macro/Youtube.py
+++ macro/Youtube.py
@@ -5,4 +5,4 @@ def execute(macro, args):
   if key.startswith('http://www.youtube.com/watch?v='):
     key = key[len('http://www.youtube.com/watch?v='):]
   key = key.replace('&', '?', 1) # query params must start with ?
-  return macro.formatter.rawHTML('<center><iframe type="text/html" width="480" height="270" src="https://www.youtube.com/embed/%(key)s" frameborder="0" allowfullscreen></iframe></center>'%locals())
+  return macro.formatter.rawHTML('<center><iframe type="text/html" width="480" height="270" src="https://www.youtube-nocookie.com/embed/%(key)s" frameborder="0" allowfullscreen></iframe></center>'%locals())
