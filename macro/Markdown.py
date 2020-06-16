5c4d9d4eb4f5cf8683ee91ea1c5edd6934a708b5
David V. Lu <davidvlu@gmail.com>
2020-06-16 18:11:50 -0400

    Client side macro parsing

--- macro/Markdown.py
+++ macro/Markdown.py
@@ -1,17 +1,7 @@
-import pypandoc
-import requests
-import re
-
-GITHUB_PATTERN = re.compile(r'https?://w*\.?github.com/([^/]+)/([^/]+)/blob/([^/]+)/(.*)')
-RAW_GITHUB_TEMPLATE = 'https://raw.githubusercontent.com/%s/%s/%s/%s'
+MODULES = """
+<script src="https://cdn.jsdelivr.net/npm/@webcomponents/webcomponentsjs@2.2.10/webcomponents-loader.min.js"></script>
+<script type="module" src="https://cdn.jsdelivr.net/gh/zerodevx/zero-md@1/src/zero-md.min.js"></script>
+"""
 
 def macro_Markdown(macro, arg1):
-    content = ''
-    if arg1 is not None:
-        m = GITHUB_PATTERN.match(arg1)
-        if m:
-            arg1 = RAW_GITHUB_TEMPLATE % m.groups()
-        r = requests.get(arg1)
-        content = r.text
-    output = pypandoc.convert_text(content, 'html', format='md')
-    return output
+    return MODULES + '<div class="markdown_macro"><zero-md src="{arg1}"></zero-md></div>'
