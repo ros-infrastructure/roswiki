ef70678246c02720b7d951399c061c1fe498b3bb
Tully Foote <tullyfoote@intrinsic.ai>
2025-02-19 09:23:49 -0800

    Update packages list link
    Fixes https://github.com/ros-infrastructure/rosindex/issues/480

--- theme/rostheme.py
+++ theme/rostheme.py
@@ -161,7 +161,7 @@ jQuery(function( $ ){
       <tr>
         <td colspan="3" height="53" width="1024"><nobr><img src="/custom/images/menu_left.png" width="17" height="53" alt=""/><a href="/"><img
         border="0" src="/custom/images/menu_documentation.png" width="237" height="53" alt="Documentation" /></a><img
-        src="/custom/images/menu_spacer.png" width="69" height="53" /><a href="https://index.ros.org/packages"><img border="0"
+        src="/custom/images/menu_spacer.png" width="69" height="53" /><a href="https://index.ros.org/?search_packages=true"><img border="0"
         src="/custom/images/menu_browse_software.png" width="268" height="53" alt="Browse Software" /></a><img
         src="/custom/images/menu_spacer.png" width="69" height="53" /><a href="https://discourse.ros.org/c/general"><img border="0"
         src="/custom/images/menu_news.png" width="84" height="53" alt="News" /></a><img
